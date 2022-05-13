5036fb21847dd0573e482eaa89c1dfe38603e5a3
Mustafa Yasin <imustafayasin@gmail.com>
2022-05-14 00:01:58 +0300

    Multiple select and refactoring

--- selectpicker.js
+++ selectpicker.js
@@ -1,34 +1,49 @@
+const SELECTPICKER = "selectpicker";
+const OPTIONS = "options";
+const HIDE = "hide";
+const OPTGROP_NODENAME = "optgroup";
+const SELECTED = "selected";
+const BUTTON_TEXT = "Choose an option"
 
 HTMLElement.prototype.selectpicker = function () {
     selectpicker_obj.setup(this);
 };
 
+const selectpicker_wrapper = document.createElement('div')
+const button = document.createElement("button")
+const options = document.createElement("div")
 var selectpicker_obj = {
     setup: function (_element) {
-        let selectpicker_wrapper = document.createElement('div');
-        selectpicker_wrapper.classList.add('selectpicker')
-        let button = document.createElement("button");
-        button.innerText = _element.options[0].innerText
+        selectpicker_wrapper.classList.add(SELECTPICKER)
+        button.innerText = BUTTON_TEXT
         selectpicker_wrapper.appendChild(button);
-        let options = document.createElement("div");
-        options.className = "options";
+        options.className = `${OPTIONS} ${HIDE}`;
         options.innerHTML = this.formatHTML(_element.innerHTML);
         selectpicker_wrapper.appendChild(options)
         _element.after(selectpicker_wrapper);
-        this.selectOption(options.querySelectorAll('option'), _element);
+        this.selectOption(options.querySelectorAll('option'), _element, this.isMultiple(_element));
+        document.addEventListener('click', this.showOrHideDropDown.bind(this, this.isMultiple(_element)))
     },
     formatHTML: function (html) {
-        return html.replaceAll('optgroup', `div class="optgroup"`)
+        return html.replaceAll(OPTGROP_NODENAME, `div class="optgroup"`)
     },
-    hasOptGroup() {
-
+    showOrHideDropDown: function (showDropdown, e) {
+        e.composedPath().includes(selectpicker_wrapper) ? (showDropdown ? options.classList.remove(HIDE) : options.classList.toggle(HIDE)) : options.classList.add(HIDE)
     },
-    selectOption(_options, _element) {
+    isMultiple: (e) => e.hasAttribute("multiple"),
+    selectOption(_options, _element, isMultiple) {
         if (!_options) return;
         [..._options].forEach(item => item.addEventListener('click', function () {
             _element.value = this.value;
-            [..._options].find(t => t.classList.contains('selected'))?.classList.remove("selected")
-            this.classList.add("selected")
+            if (isMultiple) {
+                this.classList.toggle(SELECTED);
+                button.textContent = [...options.querySelectorAll('.selected')].map(o => o.textContent).join(' + ') || BUTTON_TEXT
+                return
+            }
+            [..._options]
+                .find(t => t.classList.contains(SELECTED))?.classList.remove(SELECTED)
+            this.classList.add(SELECTED);
+            button.textContent = this.textContent
         }));
     }
 
