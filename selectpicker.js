1a492f7519e9f44a8dfd1d69ab0c019567d13d9b
Mustafa Yasin <imustafayasin@gmail.com>
2022-05-13 22:38:45 +0300

    Message

--- selectpicker.js
+++ selectpicker.js
@@ -1,32 +1,36 @@
-HTMLElement.prototype.selectpicker = function () {
 
-    var select = this;
-    const wrapper = document.createElement("div");
-    wrapper.className = "selectpicker";
-    let button = document.createElement("button");
-    button.className = "selectpicker button";
-    wrapper.appendChild(button)
-    const options = document.createElement("div");
-    options.className = "options";
-    button.innerText = [...this.options][0].innerText;
-    options.classList.add("hide");
-    button.addEventListener('click', () => {
-        options.classList.contains("hide") ? options.classList.remove("hide") : options.classList.add("hide");
-    });
-    [...this.options].forEach(item => {
-        let option = document.createElement("div");
-        option.className = "select-option";
-        option.innerText = item.innerText
-        options.appendChild(option);
-        option.addEventListener('click', function () {
-            select.value = item.value
-            this.parentNode.querySelector(".selected")?.classList.remove("selected");
-            this.classList.add("selected");
-            button.innerText = option.innerText
-            options.classList.add("hide")
-        })
-    })
-    wrapper.appendChild(options);
-    this.after(wrapper)
+HTMLElement.prototype.selectpicker = function () {
+    selectpicker_obj.setup(this);
+};
+
+var selectpicker_obj = {
+    setup: function (_element) {
+        let selectpicker_wrapper = document.createElement('div');
+        selectpicker_wrapper.classList.add('selectpicker')
+        let button = document.createElement("button");
+        button.innerText = _element.options[0].innerText
+        selectpicker_wrapper.appendChild(button);
+        let options = document.createElement("div");
+        options.className = "options";
+        options.innerHTML = this.formatHTML(_element.innerHTML);
+        selectpicker_wrapper.appendChild(options)
+        _element.after(selectpicker_wrapper);
+        this.selectOption(options.querySelectorAll('option'), _element);
+    },
+    formatHTML: function (html) {
+        return html.replaceAll('optgroup', `div class="optgroup"`)
+    },
+    hasOptGroup() {
+
+    },
+    selectOption(_options, _element) {
+        if (!_options) return;
+        [..._options].forEach(item => item.addEventListener('click', function () {
+            _element.value = this.value;
+            [..._options].find(t => t.classList.contains('selected'))?.classList.remove("selected")
+            this.classList.add("selected")
+        }));
+    }
 
 }
+
